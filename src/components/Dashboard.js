bf366b44a4aee6cf2c9e3cceb5af27cfa48f1dd5
aindriu80 <aindriu80@gmail.com>
2021-02-13 15:26:43 +0000

    Logging a user in and redirecting them to dashboard

---
+++ src/components/Dashboard.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Dashboard() {
+  return <h3>the Dashboard</h3>;
+}
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -14,6 +14,11 @@ export function AuthProvider({ children }) {
   function signup(email, password) {
     return auth.createUserWithEmailAndPassword(email, password);
   }
+
+  function login(email, password) {
+    return auth.signInWithEmailAndPassword(email, password);
+  }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
@@ -23,6 +28,7 @@ export function AuthProvider({ children }) {
   }, []);
 
   const value = {
+    login,
     currentUser,
     signup,
   };
